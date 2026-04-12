11fc1e2f452c50a81ed2cd2d487758577148d7cc
Olivia Cahyono <66972950+StrifePainter@users.noreply.github.com>
2026-04-11 23:40:05 -0400

    Custom Cursor and Trail

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 console.log("JS file is connected");
+
+
+
+
+const colors = ['#E81CFF', '#7700ff', '#ddff1b'];
+
+
+
+
+document.addEventListener('mousemove', function(e) {
+    const trail = document.createElement('div');
+    trail.className = 'trail';
+
+    const color = colors[Math.floor(Math.random() *colors.length)];
+    trail.style.background = color;
+
+    const spread = 20;
+    const offsetX = (Math.random() - 0.5) * spread;
+    const offsetY = (Math.random() - 0.5) * spread;
+
+    trail.style.left = (e.clientX + offsetX) + 'px';
+    trail.style.top = (e.clientY + offsetY) + 'px';
+
+    document.body.appendChild(trail);
+
+    setTimeout(() => {
+        trail.remove();
+    }, 800);
+
+});
